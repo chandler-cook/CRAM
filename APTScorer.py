aeafd5805fcb9e5485f3c7a2a75f562a36515ff3
JPOgnibene <142552889+JPOgnibene@users.noreply.github.com>
2024-10-17 23:41:16 -0500

    Fixed bugs and changed scoring algorithm

--- APTScorer.py
+++ APTScorer.py
@@ -1,6 +1,16 @@
 import re
 import os
 
+#the scores below need to be changed to take in the scores that are displayed after the model is initially run
+scoreP = 0
+scoreS = 0
+scoreH = 0
+
+hardwareTotal = 0
+softwareTotal = 0
+physicalTotal = 0
+checkedAPTs = [] #this list needs to be filled with the checked APTs
+
 # Function to parse the file and extract numbers next to specified keywords
 def parse_file(file_path):
     # Initialize variables to store the values
@@ -32,38 +42,72 @@ def parse_file(file_path):
         if physical_match:
             physical_value = int(physical_match.group(1))
     return hardware_value, software_value, physical_value
+
 def process_directory(directory_path):
-    hardwareTotal = 0
-    softwareTotal = 0
-    physicalTotal = 0
-    for filename in os.listdir(directory_path):
-        # Check if the file name contains '-Profile'
-        if '-Profile' in filename:
-            file_path = os.path.join(directory_path, filename)
-            # Call parse_file function and retrieve values
-            hardware, software, physical = parse_file(file_path)
-        hardwareTotal = hardwareTotal + hardware
-        softwareTotal = softwareTotal + software
-        physicalTotal = physicalTotal + physical
+    global hardwareTotal
+    global softwareTotal
+    global physicalTotal
+    hardware, software, physical = parse_file(directory_path)
+    hardwareTotal = hardwareTotal + hardware
+    softwareTotal = softwareTotal + software
+    physicalTotal = physicalTotal + physical
     return hardwareTotal, softwareTotal, physicalTotal
 
 def makeNewScore(hardware, software, physical):
-    scoreP = physical - (2 * (scoreP/10))
-    scoreH = hardware - (2 * (scoreH/10))
-    scoreS = software - (2 * (scoreS/10))
+    global scoreH,scoreP,scoreS,checkedAPTs
+    if(len(checkedAPTs) <= 5):
+        scoreP = scoreP - (physical * (1/5))
+        scoreH = scoreH - (hardware * (1/5))
+        scoreS = scoreS - (software * (1/5))
+    elif(len(checkedAPTs) <= 10):
+        scoreP = scoreP - (physical * (1/10))
+        scoreH = scoreH - (hardware * (1/10))
+        scoreS = scoreS - (software * (1/10))
+    elif(len(checkedAPTs) <= 20):
+        scoreP = scoreP - (physical * (1/20))
+        scoreH = scoreH - (hardware * (1/20))
+        scoreS = scoreS - (software * (1/20))
+    elif(len(checkedAPTs) <= 30):
+        scoreP = scoreP - (physical * (1/30))
+        scoreH = scoreH - (hardware * (1/30))
+        scoreS = scoreS - (software * (1/30))
+    elif(len(checkedAPTs) <= 40):
+        scoreP = scoreP - (physical * (1/40))
+        scoreH = scoreH - (hardware * (1/40))
+        scoreS = scoreS - (software * (1/40))
+    elif(len(checkedAPTs) <= 50):
+        scoreP = scoreP - (physical * (1/50))
+        scoreH = scoreH - (hardware * (1/50))
+        scoreS = scoreS - (software * (1/50))
+    elif(len(checkedAPTs) <= 75):
+        scoreP = scoreP - (physical * (1/75))
+        scoreH = scoreH - (hardware * (1/75))
+        scoreS = scoreS - (software * (1/75))
+    elif(len(checkedAPTs) <= 100):
+        scoreP = scoreP - (physical * (1/100))
+        scoreH = scoreH - (hardware * (1/100))
+        scoreS = scoreS - (software * (1/100))
+    elif(len(checkedAPTs) <= 150):
+        scoreP = scoreP - (physical * (1/150))
+        scoreH = scoreH - (hardware * (1/150))
+        scoreS = scoreS - (software * (1/150))
+    else:
+        scoreP = scoreP - (physical * (1/200))
+        scoreH = scoreH - (hardware * (1/200))
+        scoreS = scoreS - (software * (1/200))
+    scoreP = round(scoreP)
+    scoreH = round(scoreH)
+    scoreS = round(scoreS)
+    print("Physical: " + str(scoreP) + "   Hardware: " + str(scoreH) + "   Software: " + str(scoreS))
+    return int(scoreS),int(scoreP),int(scoreH)
 # Example usage
 
-scoreP = 0
-scoreS = 0
-scoreH = 0
-checkedAPTs = []
+hardware = 0
+physical = 0
+software = 0
 
-directory = "C:\\Users\\jippy\\OneDrive\\Desktop\\CSC Work\\Cyber\\CRAM Data\\Text Files\\"
+directory = "C:\\Users\\jippy\\OneDrive\\Desktop\\CSC Work\\Cyber\\CRAM Data\\Text Files\\" #this path needs to be changed to point to wherever the text files for the APTs are stored
 for x in checkedAPTs:
-    for filename in os.listdir(directory):
-        if x in filename and '-Profile' in filename:
-            hardware,software,physical = process_directory(os.path.join(directory, x))
-hardware = hardware/2
-software = software/2
-physical = physical/2
-makeNewScore(hardware, software, physical)
+    directory_tmp = directory + x + '-Profile.txt'
+    hardware,software,physical = process_directory(directory_tmp)
+scoreS,scoreP,scoreH = makeNewScore(hardware, software, physical) #these are the scores that have been updated to reflect the APT data
