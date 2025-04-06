74578b994103cca47de048cd4a2a51b7eb4b8b86
Chandler Cook <142535965+chandler-cook@users.noreply.github.com>
2025-04-06 18:46:15 -0500

    Delete checkFileName.py

--- checkFileName.py
+++
@@ -1,7 +0,0 @@
-def checkName(filename):
-    filepath = ""
-    if "systemOne" in filename:
-        filepath = "path to Sys1Crit.csv"
-    if "systemTwo" in filename:
-        filepath = "path to Sys2Crit.csv"
-    return filepath
