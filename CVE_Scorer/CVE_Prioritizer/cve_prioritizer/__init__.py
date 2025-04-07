54f3df803c644fc526b3f1227413d7d4ab4f09de
Chandler Cook <142535965+chandler-cook@users.noreply.github.com>
2025-04-07 09:20:02 -0500

    Delete CVE_Scorer directory

--- CVE_Scorer/CVE_Prioritizer/cve_prioritizer/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-__author__ = "Mario Rojas"
-__license__ = "BSD 3-clause"
-__version__ = "1.6.4"
-__maintainer__ = "Mario Rojas"
-__status__ = "Production"
-
-from .cve_prioritizer import main
